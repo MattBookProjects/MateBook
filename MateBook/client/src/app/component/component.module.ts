e993600f329bd0742b0d7baf5e17c24818e1f17f
unknown <mateuszksiazek44@gmail.com>
2023-02-07 19:55:24 +0100

    Register success bug fix part two

--- MateBook/client/src/app/component/component.module.ts
+++ MateBook/client/src/app/component/component.module.ts
@@ -7,10 +7,12 @@ import { NavbarComponent } from "./navbarComponent/navbar.component";
 import { NavbarModule } from "./navbarComponent/navbar.module";
 import { RegisterPageComponent } from "./registerPage/register-page.component";
 import { RegisterPageModule } from "./registerPage/register-page.module";
+import { RegisterSuccessComponent } from "./registerSuccess/register-success.component";
+import { RegisterSuccessModule } from "./registerSuccess/registerSuccess.module";
 
 
 @NgModule({
-    imports: [LoginPageModule, NavbarModule, RegisterPageModule, ErrorModule],
-    exports: [LoginPageComponent, NavbarComponent, RegisterPageComponent, ErrorComponent]
+    imports: [LoginPageModule, NavbarModule, RegisterPageModule, ErrorModule, RegisterSuccessModule],
+    exports: [LoginPageComponent, NavbarComponent, RegisterPageComponent, ErrorComponent, RegisterSuccessComponent]
 })
 export class ComponentModule { }
