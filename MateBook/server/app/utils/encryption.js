6b1796f08618f4a82dc671909bd5e125bf07a081
unknown <mateuszksiazek44@gmail.com>
2023-02-17 00:53:32 +0100

    session tokens are now generated randomly

--- MateBook/server/app/utils/encryption.js
+++ MateBook/server/app/utils/encryption.js
@@ -2,7 +2,13 @@ import { createHash } from 'crypto';
 
 function Encryption(){
     this.generateAuthToken = () => {
-        return '00000000'
+        const symbols = '0123456789abcdef';
+        let token = ''
+        for (let i = 0; i < 16; i++){
+            const index = Math.floor(Math.random() * 16);
+            token = token + symbols.charAt(index)
+        }
+        return token;
     };
     this.hashPassword = (password) => {
         return createHash('sha256').update(password).digest('hex');
