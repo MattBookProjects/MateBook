4470bc680ec041c4bddce3213937a18816bc4e58
unknown <mateuszksiazek44@gmail.com>
2023-03-05 12:31:02 +0100

    Refactored database

--- MateBook/server/app/database/sqlHelpers.js
+++ MateBook/server/app/database/sqlHelpers.js
@@ -33,6 +33,18 @@ function SqlHelpers(){
         keys.forEach(key => object[key] = row[key].value);
         return object;
     };
+    this.getSqlUpdateStatementFromObject = (object) => {
+        if (typeof row !== 'object' || Array.isArray(row)){
+            throw 'NonObjectInputError';
+        }
+        let keys = Object.keys(object);
+        if ( keys.length === 0){
+           throw 'EmptyObjectInputError'
+        }
+        let values = [];
+        keys.forEach(key => values.push(`${key}=${object[key]}`));
+        return values.join(', ')
+    }
 };
 
 const sqlHelpers = new SqlHelpers();
